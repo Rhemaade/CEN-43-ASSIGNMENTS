7af8aeee0f061e07c2b3476c0e2d086c64c4bd5c
ADEFARAKAN RHEMA TENIADE <rhemaadefarakan@gmail.com>
2025-01-18 09:28:48 +0100

    Add files via upload

---
+++ CLASSWORK 2.sql
@@ -0,0 +1,37 @@
+USE [OMORUYI CLASSWORK 434 1]
+--INSERT INTO Table_for_2 (ID,Matric_no,Name,Gender,Score)
+--VALUES 
+--(1,'21CJ029173','Adefarakan Rhema','F',98),
+--(2, '21CJ029174', 'Adewole Obalabi', 'M', 76),
+--(3, '21CJ029190', 'Ayininuola Oluwatoyin', 'F', 100),
+--(4, '21CJ029225', 'Nwabu Nwosu Ngozi', 'F', 96),
+--(5, '21CJ029188', 'AMOS Ibala', 'F', 88),
+--(6, '21CJ029192', 'Ayoola Oreofe', 'F', 90),
+--(7, '21CJ029191', 'Ayoola Olumide', 'M', 85),
+--(8, '21CJ029199', 'Egere Joshua', 'M', 92),
+--(9, '21CJ029236', 'Itshoumole Itsehme', 'F', 87),
+--(10, '21CJ029131', 'Ogunmola David', 'M', 79)
+
+
+--SELECT Name, Matric_no FROM Table_for_2
+
+
+--SELECT Name, Matric_no FROM Table_for_2 where Gender= 'F' and Score > 90
+
+--SELECT * FROM Table_for_2 where Gender= 'F' and Score > 80 ORDER BY Score ASC
+
+--UPDATE Table_for_2
+--SET Score = 99
+--Where ID = 3
+
+----SELECT * From Table_for_2
+
+--SELECT MAX(Score) from Table_for_2
+
+--SELECT MIN(Score) from Table_for_2
+
+--SELECT Score FROM Table_for_2
+
+
+
+Go
---
+++ Classwork 1.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "student_table" (
+"ID" INT,
+"Matric_No" varchar(20),
+"Name" varchar(50),
+"Gender" varchar(6)
+);
+
+SELECT * FROM student_table;
+
+INSERT INTO student_table 
+VALUES (2101252, '21CJ029190', 'Toyin Desola', 'Female'),
+(2101103, '21AI028977', 'Usman Oshone', 'Female'),
+(2102350, '21CG029856', 'Tomi China', 'Female'),
+(2100288, '21CK029324', 'Olotu Bimpe', 'Female'),
+(2102912, '21AI028967', 'Eneh Winnie', 'Female'),
+(2100063, '21CJ029215', 'Genesis Grace', 'Female'),
+(2100314, '21CJ029192', 'Ayoola Ore', 'Female'),
+(2102776, '21CJ029191', 'Ayoola Olumide', 'Male'),
+(2102340, '21CJ029225', 'Ngozi Nwabu', 'Female'),
+(2100677, '21CG029213', 'Iyamu Wisdom', 'Male');
+
+
